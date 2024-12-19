8250cca271b1321ca3f3265343253834df7cc7f9
ChenCiaLiang <aliang334chen@gmail.com>
2024-12-19 23:06:50 +0700

    Fix Python #2

--- MainStreamlit_B_Scikit-Learn.py
+++ MainStreamlit_B_Scikit-Learn.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = r'model_mobilenet.h5'
+model = load_model('model_mobilenet.h5')
 class_names = ['Matang', 'Mentah', 'Setengah Matang']
 
 # Function to preprocess and classify image
